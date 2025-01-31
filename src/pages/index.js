704236e5a9c532ef293bb88d3cd9b1a1a9ad15b2
Swastik Chakraborty <hello@swastik.dev>
2025-01-31 20:51:46 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 import Mission from '@site/src/components/Aboutpage/Mission';
 import Reach from '@site/src/components/Aboutpage/Reach';
 import Founder from '@site/src/components/Aboutpage/Founder';
-import Newsletter from '../newsletter/Newsletter';
+import Newsletter from '@site/src/components/newsletter/Newsletter';
 
 
 export default function Home() {
