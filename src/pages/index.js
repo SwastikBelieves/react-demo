ead1e0e622c9215a764ba8c676d01a3626706275
Swastik Chakraborty <hello@swastik.dev>
2025-01-31 20:46:43 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,9 +15,9 @@ export default function Home() {
       {/* <HomepageHeader /> */}
       <main>
         <HomepageFeatures />
-        <Founder />
         <Mission />
         <Reach />
+        <Founder />
         <Newsletter />
       </main>
     </Layout>
