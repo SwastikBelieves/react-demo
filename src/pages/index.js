efbf450de03722926725dd52ba999c83512b5150
Swastik Chakraborty <hello@swastik.dev>
2025-01-31 19:37:50 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import Mission from '@site/src/components/Aboutpage/Mission';
+import Reach from '@site/src/components/Aboutpage/Reach';
+import Founder from '@site/src/components/Aboutpage/Founder';
+import Layout from '@theme/Layout';
+import Team from '../components/team/Team';
 
 
 export default function Home() {
@@ -12,6 +17,10 @@ export default function Home() {
       <main>
         <HomepageFeatures />
       </main>
+      <Mission/>
+      <Reach />
+      <Founder />
+      <Team heading={"Kubesimplify Ambassadors"} slider={true} />
     </Layout>
   );
 }
