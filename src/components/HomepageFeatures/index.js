aeb241790148093d560fbbb3b80dbf24b3077620
Swastik Chakraborty <hello@swastik.dev>
2025-02-01 21:36:20 +0530

    Update index.js

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -22,7 +22,8 @@ import workshop from "./assests/workshop.png";
 
 export default function HomepageFeatures() {
   return (
-    <section className={`${styles.features} main_content`}>
+    <div className="main_content">
+    <section className={styles.features}>
       <div className={styles.hp}>
         {/* first portion(container) of the homepage */}
         <div className={styles.hp_cont_1}>
@@ -268,5 +269,6 @@ export default function HomepageFeatures() {
         
       </div>
     </section>
+    </div>
   );
 }
